4ae0f630ad3b77315106893ae614ab720b1dc22c
Jay Young <jaytyoung1@gmail.com>
2022-07-25 23:03:49 -0400

    Issue-1: Update bloc to v8

--- lib/application/fantasy_team/fantasy_team_bloc.dart
+++ lib/application/fantasy_team/fantasy_team_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
-import 'package:equatable/equatable.dart';
+
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:injectable/injectable.dart';
 import 'package:lod_prize_tracker/domain/fantasy_team/fantasy_team.dart';
@@ -11,35 +12,31 @@ part 'fantasy_team_state.dart';
 @injectable
 class FantasyTeamBloc extends Bloc<FantasyTeamEvent, FantasyTeamState> {
   FantasyTeamBloc({
-    required IFantasyTeamRepository fantasyTeamRepository,
-  })  : _fantasyTeamRepository = fantasyTeamRepository,
-        super(const FantasyTeamStateInitial());
-
-  final IFantasyTeamRepository _fantasyTeamRepository;
-
-  @override
-  Stream<FantasyTeamState> mapEventToState(FantasyTeamEvent event) async* {
-    if (event is LoadFantasyTeams) {
-      yield* _mapLoadFantasyTeamsToState();
-    }
+    required this.fantasyTeamRepository,
+  }) : super(const FantasyTeamStateInitial()) {
+    on<LoadFantasyTeams>((event, emit) => loadFantasyTeamsToState(emit));
   }
 
-  Stream<FantasyTeamState> _mapLoadFantasyTeamsToState() async* {
-    yield FantasyTeamStateLoading();
+  final IFantasyTeamRepository fantasyTeamRepository;
+
+  FutureOr<void> loadFantasyTeamsToState(
+    Emitter<FantasyTeamState> emit,
+  ) async {
+    emit(FantasyTeamStateLoading());
 
     List<FantasyTeam> _fantasyTeamsRanked =
-        await _fantasyTeamRepository.getFantasyTeamsRanked();
+        await fantasyTeamRepository.getFantasyTeamsRanked();
 
     List<FantasyTeam> _fantasyTeamsPointsFor =
-        await _fantasyTeamRepository.getPointsFor();
+        await fantasyTeamRepository.getPointsFor();
 
     List<FantasyTeam> _fantasyTeamsPointsAllowed =
-        await _fantasyTeamRepository.getPointsAllowed();
+        await fantasyTeamRepository.getPointsAllowed();
 
-    yield FantasyTeamStateLoaded(
+    emit(FantasyTeamStateLoaded(
       fantasyTeamsRanked: _fantasyTeamsRanked,
       fantasyTeamsPointsFor: _fantasyTeamsPointsFor,
       fantasyTeamsPointsAllowed: _fantasyTeamsPointsAllowed,
-    );
+    ));
   }
 }
